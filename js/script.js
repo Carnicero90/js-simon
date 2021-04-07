850f759649c8c48328fcc7e1b706c4e242b19ea2
Carnicero90 <filippomontani82@gmail.com>
2021-04-07 13:36:25 +0200

    primo commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var randNums = Array.from({
+    length: 5
+}, i => getRandomInt(1, 11));
+
+alert(`numeri da memorizzare:\n${randNums.join('\n')}`)
+setTimeout(function() {
+    var rightCalls = [];
+    var output = '';
+    var counter = 0;
+    for (var i = 0; i < randNums.length; i++) {
+        guess = parseInt(prompt(`inserire n.ro ${i+1}`)) === randNums[i];
+        counter += guess;
+        rightCalls.push(guess);
+        output += `${guess ? '' : 'non '}hai indovinato l'elemento al ${i+1}° posto\n`
+    }
+    alert(output);
+    alert(`totale n.ri indovinati: ${counter}`);
+
+}, 3000)
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+}
