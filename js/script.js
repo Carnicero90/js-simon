065c8e4028dc3f64b7aae84a4ca08300dbc135ca
Carnicero90 <filippomontani82@gmail.com>
2021-04-07 14:41:09 +0200

    test di sorta di animazione

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ var randNums = Array.from({
     length: 5
 }, i => getRandomInt(1, 11));
 
-alert(`numeri da memorizzare:\n${randNums.join('\n')}`)
+for (var i = 0; i < randNums.length; i++) {
+    document.getElementsByTagName('li')[i].innerHTML = randNums[i];
+}
+
 setTimeout(function() {
+    for (var item of document.getElementsByTagName('li')) {
+        item.classList.add('pause')
+    }
     var rightCalls = [];
     var output = '';
     var counter = 0;
@@ -14,9 +20,24 @@ setTimeout(function() {
         output += `${guess ? '' : 'non '}hai indovinato l'elemento al ${i+1}° posto\n`
     }
     alert(output);
-    alert(`totale n.ri indovinati: ${counter}`);
+    document.getElementById('punteggio').innerHTML += counter;
+}, 4900)
 
-}, 3000)
+// alert(`numeri da memorizzare:\n${randNums.join('\n')}`)
+// setTimeout(function() {
+//     var rightCalls = [];
+//     var output = '';
+//     var counter = 0;
+//     for (var i = 0; i < randNums.length; i++) {
+//         guess = parseInt(prompt(`inserire n.ro ${i+1}`)) === randNums[i];
+//         counter += guess;
+//         rightCalls.push(guess);
+//         output += `${guess ? '' : 'non '}hai indovinato l'elemento al ${i+1}° posto\n`
+//     }
+//     alert(output);
+//     alert(`totale n.ri indovinati: ${counter}`);
+
+// }, 3000)
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
