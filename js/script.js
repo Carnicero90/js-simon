e575b4fe3e79e25749d6f1282fce527e4830d714
Carnicero90 <filippomontani82@gmail.com>
2021-04-07 15:02:57 +0200

    cambiamenti ancora sostaanzialmente solo grafici'

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ var randNums = Array.from({
 }, i => getRandomInt(1, 11));
 
 for (var i = 0; i < randNums.length; i++) {
+    // randNums.push(getRandomInt(1, 11));
     document.getElementsByTagName('li')[i].innerHTML = randNums[i];
 }
 
 setTimeout(function() {
-    for (var item of document.getElementsByTagName('li')) {
-        item.classList.add('pause')
-    }
+    document.getElementById('deck').classList.add('pause');
     var rightCalls = [];
     var output = '';
     var counter = 0;
@@ -22,22 +21,6 @@ setTimeout(function() {
     alert(output);
     document.getElementById('punteggio').innerHTML += counter;
 }, 4900)
-
-// alert(`numeri da memorizzare:\n${randNums.join('\n')}`)
-// setTimeout(function() {
-//     var rightCalls = [];
-//     var output = '';
-//     var counter = 0;
-//     for (var i = 0; i < randNums.length; i++) {
-//         guess = parseInt(prompt(`inserire n.ro ${i+1}`)) === randNums[i];
-//         counter += guess;
-//         rightCalls.push(guess);
-//         output += `${guess ? '' : 'non '}hai indovinato l'elemento al ${i+1}° posto\n`
-//     }
-//     alert(output);
-//     alert(`totale n.ri indovinati: ${counter}`);
-
-// }, 3000)
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
