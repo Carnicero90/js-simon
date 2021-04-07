757c847d561e19887342b8a14c0ed9ace8ccd8a5
Carnicero90 <filippomontani82@gmail.com>
2021-04-07 19:11:39 +0200

    cominciata opera di riordino/commento script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,59 @@
-var randNums = Array.from({
-    length: 5
-}, i => getRandomInt(1, 11));
+// HTML ELEMENTS
+const DECK = document.getElementById('deck');
+const QUESTION = document.getElementById('question');
+const SCORE = document.getElementById('punteggio');
+const INPUT_WRAPPER = document.getElementById('prompt-wrapper');
+const USER_INPUT = document.getElementById('prompt-number');
 
+// MISC
+const SEED = 5; // number of cards/random integers
 
-const boh = document.getElementById('boh');
-const userPrompt = document.getElementById('question');
-for (var i = 0; i < randNums.length; i++) {
-    // randNums.push(getRandomInt(1, 11));
-    document.getElementsByTagName('li')[i].innerHTML = randNums[i];
+/*  FOR (N == SEED) TIMES: 
+ * GENERATE item = RANDOM INTEGER, 
+ * CREATE li>{item} IN DECK, 
+ * PUSH item TO randNums ARRAY
+ */
+var randNums = [];
+for (var i = 0; i < SEED; i++) {
+    var item = getRandomInt(1, 11);
+    DECK.innerHTML += `<li>${item}</li>`
+    randNums.push(item);
 }
-
+/* HIDE DECK>li INNERHTML AFTER A 5s (MORE OR LESS) DELAY,
+    DISPLAY INPUT_WRAPPER
+*/
 setTimeout(function() {
-    document.getElementById('deck').classList.add('pause');
-    boh.classList.remove('nada');
+    DECK.classList.add('pause');
+    INPUT_WRAPPER.classList.remove('nada');
 }, 4900)
+
 var counter = 0;
 var rightCalls = [];
 var output = '';
 
-userPrompt.innerHTML = `inserire n.ro ${counter+1}`
+QUESTION.innerHTML = `inserire n.ro ${counter+1}`
 document.getElementById('send').addEventListener('click', function() {
-        var answer = parseInt(document.getElementById('prompt-number').value) === randNums[counter];
+        var answer = parseInt(USER_INPUT.value) === randNums[counter];
         counter++;
         rightCalls.push(answer);
         if (counter == 5) {
             alert(rightCalls);
-            boh.classList.add('nada');
-            document.getElementById('deck').classList.remove('pause');
+            INPUT_WRAPPER.classList.add('nada');
+            DECK.classList.remove('pause');
             // robe orripilanti qua, stasera miglioro
-            document.getElementById('punteggio').innerHTML += rightCalls.reduce(function(a, b) {
+            SCORE.innerHTML += rightCalls.reduce(function(a, b) {
                 return a + b
-
             }, 0);
         }
-        userPrompt.innerHTML = `inserire n.ro ${counter+1}`
-
-
+        QUESTION.innerHTML = `inserire n.ro ${counter+1}`
     }
 
 )
 
+// GENERIC FUNCTIONS
 function getRandomInt(min, max) {
+    // RETURN A RANDOM INTEGER BETWEEN min (INCLUDED) AND max (EXCLUDED)
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+    return Math.floor(Math.random() * (max - min)) + min;
 }
